67a6c17ba25509a3de93cb92a72acbbddf2ba15b
origanoinitalian <avishanabdinejad@gmail.com>
2026-04-22 17:52:09 +0200

    fix[parser]:just the runner engine

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-import requests
-
-def get_posts():
-    url="https://api.chess.com/pub/player/hikaru"
-
-    try:
-        headers = {
-            "User-Agent": "My-chess-proj/1.0"
-        }
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            posts = response.json()
-            return posts
-        else:
-            print('Error:', response.status_code)
-            return None
-        
-    except requests.exceptions.RequestException as e:
-        print('Error:', e)
-        return None
+from fetcher import fetch_data
+from parser import parse_data
 
 def main():
-    posts = get_posts()
-    if posts:
-        print(posts)
+    data = fetch_data()
+    parsed_data = parse_data(data)
+    if parsed_data:
+        print(parsed_data)
 
 
 if __name__ == "__main__":
